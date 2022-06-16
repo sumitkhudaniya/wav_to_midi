b9d5f70c40d6390cf590759fb5820cc4e2f06fe8
Deepak <latherdeepak6@gmail.com>
2022-06-16 10:54:16 -0400

    Initial commit

---
+++ wav_to_midi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wav_to_midi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wav_to_midi.settings')
+
+application = get_asgi_application()
---
+++ wav_to_midi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wav_to_midi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wav_to_midi.settings')
+
+application = get_wsgi_application()
